d0a38425444af7a7ee0b9643ca602ab8f463e906
junsik <deeptree97@gmail.com>
2025-03-25 00:05:51 +0900

    revert: df35624 커밋 되돌림

--- community/src/main/java/com/example/community/controller/TestController.java
+++ community/src/main/java/com/example/community/controller/TestController.java
@@ -1,33 +1,14 @@
 package com.example.community.controller;
 
-import com.example.community.service.TestService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
 @RestController
 @RequestMapping("/api")
 public class TestController {
-    private final TestService testService;
-
-    public TestController(TestService testService) {
-        this.testService = testService;
-    }
-
-    @GetMapping("/test-db")
-    public List<String> getNamesFromDb() {
-        return testService.getAllNames();
-    }
-
     @GetMapping("/hello")
     public String hello() {
         return "Hello, Spring Boot!";
-    }
-
-    @GetMapping("/test")
-    public String test() {
-        return testService.getMessage();
     }
 }

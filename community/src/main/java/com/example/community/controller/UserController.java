e310f3d7775b519c299fd31ee80b04ce2389844f
junsik <deeptree97@gmail.com>
2025-03-25 23:31:58 +0900

    feat: 비밀번호 변경, 회원 정보수정, 회원탈퇴 분리

---
+++ community/src/main/java/com/example/community/controller/UserController.java
@@ -0,0 +1,25 @@
+package com.example.community.controller;
+
+import com.example.community.dto.request.UpdateMemberRequestDto;
+import com.example.community.dto.response.ApiResponse;
+import com.example.community.service.AuthService;
+import com.example.community.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+    private final AuthService authService;
+
+    @PutMapping("/me")
+    public ResponseEntity<ApiResponse<Void>> updateProfile(@RequestBody UpdateMemberRequestDto request,
+                                                           @AuthenticationPrincipal UserDetails userDetails) {
+        authService.
+    }
+}
--- community/src/main/java/com/example/community/service/UserService.java
+++ community/src/main/java/com/example/community/service/UserService.java
@@ -1,59 +1,37 @@
 package com.example.community.service;
 
+import com.example.community.dto.request.UpdateMemberRequestDto;
+import com.example.community.model.Member;
 import com.example.community.model.User;
+import com.example.community.respository.MemberRepository;
 import com.example.community.respository.UserRepository;
 //import com.example.community.security.JwtUtil;
+import com.example.community.security.JwtUtil;
 import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
 @Service
+@RequiredArgsConstructor
 public class UserService {
-//    private final UserRepository userRepository;
-//    private final PasswordEncoder passwordEncoder;
-////    private final JwtUtil jwtUtil;
-//
-//    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder, JwtUtil jwtUtil) {
-//        this.userRepository = userRepository;
-//        this.passwordEncoder = passwordEncoder;
-//        this.jwtUtil = jwtUtil;
-//    }
-//
-//    // ✅ 회원가입
-//    @Transactional
-//    public User registerUser(User user) {
-//        user.setPassword(passwordEncoder.encode(user.getPassword())); // 비밀번호 해싱
-//        return userRepository.save(user);
-//    }
-//
-//    // ✅ 로그인 (JWT 발급)
-//    public String login(String email, String password) {
-//        User user = userRepository.findByEmail(email)
-//                .orElseThrow(() -> new IllegalArgumentException("이메일이 존재하지 않습니다."));
-//
-//        if (!passwordEncoder.matches(password, user.getPassword())) {
-//            throw new IllegalArgumentException("비밀번호가 틀렸습니다.");
-//        }
-//
-//        return jwtUtil.generateToken(user.getEmail()); // JWT 토큰 발급
-//    }
+    private final MemberRepository memberRepository;
 //
 //    // ✅ 회원 정보 조회
 //    public Optional<User> getUserByEmail(String email) {
 //        return userRepository.findByEmail(email);
 //    }
 //
-//    // ✅ 회원 정보 수정 (닉네임, 프로필 사진)
-//    @Transactional
-//    public User updateUser(Long userId, String nickname, String profileImage) {
-//        User user = userRepository.findById(userId)
-//                .orElseThrow(() -> new IllegalArgumentException("사용자가 존재하지 않습니다."));
-//        user.setNickname(nickname);
-//        user.setProfileImage(profileImage);
-//        return userRepository.save(user);
-//    }
+    // 회원 정보 수정 (닉네임, 프로필 사진)
+    @Transactional
+    public void updateProfile(String email, UpdateMemberRequestDto request) {
+        Member member = memberRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("유저가 존재하지 않습니다."));
+
+    }
 //
 //    // ✅ 비밀번호 변경
 //    @Transactional

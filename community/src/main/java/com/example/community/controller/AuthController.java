283e1a4f76766057d648256385040e024a6a2651
junsik <deeptree97@gmail.com>
2025-03-28 00:01:04 +0900

    modify: 회원가입 시 전송 객체 변경(json -> form-data)

--- community/src/main/java/com/example/community/controller/AuthController.java
+++ community/src/main/java/com/example/community/controller/AuthController.java
@@ -5,13 +5,13 @@ import com.example.community.dto.request.SignupRequestDto;
 import com.example.community.dto.response.ApiResponse;
 import com.example.community.dto.response.TokenResponseDto;
 import com.example.community.service.AuthService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.Map;
 
@@ -21,9 +21,11 @@ import java.util.Map;
 public class AuthController {
     private final AuthService authService;
 
-    @PostMapping("/signup")
-    public ResponseEntity<ApiResponse<Map<String, Long>>> signup(@RequestBody SignupRequestDto request) {
-        Long userId = authService.signup(request);
+    @PostMapping(value = "/signup", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<ApiResponse<Map<String, Long>>> signup(
+            @Valid @RequestPart("request") SignupRequestDto request,
+            @RequestPart(value = "image", required = false) MultipartFile image) {
+        Long userId = authService.signup(request, image);
         Map<String, Long> data = Map.of("user_id", userId);
 
         return ResponseEntity.status(HttpStatus.CREATED)

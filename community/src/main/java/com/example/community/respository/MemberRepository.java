213b0e068fe5d4a9269aad158daf60b8fbcdbab1
junsik <deeptree97@gmail.com>
2025-03-27 23:55:34 +0900

    modify: soft-delete 적용에 따른 Member 조회 메서드 변경

--- community/src/main/java/com/example/community/respository/MemberRepository.java
+++ community/src/main/java/com/example/community/respository/MemberRepository.java
@@ -6,6 +6,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByIdAndDeletedFalse(Long id);
     Optional<Member> findByEmail(String email); // 중복가입 방지
     Optional<Member> findByNickname(String nickname); // 중복 닉네임 방지
+    Optional<Member> findByEmailAndDeletedFalse(String email);
+    boolean existsByIdAndDeletedFalse(Long id);
+    boolean existsByEmailAndDeletedFalse(String email);
+    boolean existsByNicknameAndDeletedFalse(String nickname);
 }

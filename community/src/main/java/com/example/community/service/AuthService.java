3d0f06d953d3361608d45e194cb36353764342a4
junsik <deeptree97@gmail.com>
2025-03-27 23:53:56 +0900

    modify: soft-delete로 DB 변경에 따른 중복 검사 로직 변경

--- community/src/main/java/com/example/community/service/AuthService.java
+++ community/src/main/java/com/example/community/service/AuthService.java
@@ -3,6 +3,8 @@ package com.example.community.service;
 import com.example.community.dto.request.LoginRequestDto;
 import com.example.community.dto.request.SignupRequestDto;
 import com.example.community.dto.response.TokenResponseDto;
+import com.example.community.exception.ImageUploadException;
+import com.example.community.global.enums.UploadType;
 import com.example.community.jwt.JwtProvider;
 import com.example.community.model.Member;
 import com.example.community.respository.MemberRepository;
@@ -10,6 +12,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @Service
 @RequiredArgsConstructor
@@ -17,35 +22,48 @@ public class AuthService {
     private final MemberRepository memberRepository;
     private final PasswordEncoder passwordEncoder;
     private final JwtProvider jwtProvider;
+    private final FileService fileService;
 
     @Transactional
-    public Long signup(SignupRequestDto request) {
-        if (memberRepository.findByEmail(request.getEmail()).isPresent()) {
-            throw new IllegalArgumentException("이미 존재하는 이메일입니다.");
+    public Long signup(SignupRequestDto request, MultipartFile image) {
+        String profileImageUrl = "/images/profile/default-image.png"; // 기본 경로 (image가 null이면)
+
+        if (memberRepository.existsByEmailAndDeletedFalse(request.getEmail())) {
+            throw new IllegalArgumentException("이미 사용 중인 이메일입니다.");
         }
 
-        if (memberRepository.findByNickname(request.getNickname()).isPresent()) {
-            throw new IllegalArgumentException("중복된 닉네임입니다.");
+        if (memberRepository.existsByNicknameAndDeletedFalse(request.getNickname())) {
+            throw new IllegalArgumentException("이미 사용 중인 닉네임입니다.");
         }
 
-        Member member = new Member();
-        member.setEmail(request.getEmail());
-        member.setPassword(passwordEncoder.encode(request.getPassword()));
-        member.setNickname(request.getNickname());
-        member.setProfileImageUrl(request.getProfileImageUrl());
+        if (image != null && !image.isEmpty()) {
+            try {
+                profileImageUrl = fileService.saveImage(image, UploadType.PROFILE);
+
+            } catch (Exception e) {
+                throw new ImageUploadException("이미지 저장 실패");
+            }
+        }
+
+        Member member = Member.builder()
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .nickname(request.getNickname())
+                .profileImageUrl(profileImageUrl)
+                .build();
 
         return memberRepository.save(member).getId();
     }
 
     public TokenResponseDto login(LoginRequestDto request) {
-        Member member = memberRepository.findByEmail(request.getEmail())
+        Member member = memberRepository.findByEmailAndDeletedFalse(request.getEmail())
                 .orElseThrow(() -> new IllegalArgumentException("계정을 찾을 수 없습니다."));
 
         if (!passwordEncoder.matches(request.getPassword(), member.getPassword())) {
             throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
         }
 
-        String token = jwtProvider.generateToken(member.getEmail());
-        return new TokenResponseDto(token);
+        String accessToken = jwtProvider.generateToken(member.getId());
+        return new TokenResponseDto(accessToken);
     }
 }

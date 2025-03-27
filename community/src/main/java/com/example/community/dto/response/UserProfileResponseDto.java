c9509fbac0751eaa8a51d136220b33f5a383ab3d
junsik <deeptree97@gmail.com>
2025-03-28 00:02:16 +0900

    modify: 생성자 형식 build로 변경

--- community/src/main/java/com/example/community/dto/response/UserProfileResponseDto.java
+++ community/src/main/java/com/example/community/dto/response/UserProfileResponseDto.java
@@ -1,22 +1,23 @@
 package com.example.community.dto.response;
 
-import com.example.community.model.User;
+import com.example.community.model.Member;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Builder
 @AllArgsConstructor
 public class UserProfileResponseDto {
-    private Long id;
+    private String email;
     private String nickname;
     private String profileImageUrl;
 
-    public static UserProfileResponseDto fromEntity(User user) {
-        return new UserProfileResponseDto(
-                user.getId(),
-                user.getNickname(),
-                user.getProfileImageUrl()
-        );
+    public static UserProfileResponseDto of(Member member) {
+        return UserProfileResponseDto.builder()
+                .email(member.getEmail())
+                .nickname(member.getNickname())
+                .profileImageUrl(member.getProfileImageUrl())
+                .build();
     }
-
 }

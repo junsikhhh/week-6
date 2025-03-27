cbe5f17df9f817aaf695f3e8656ac0959c8fa096
junsik <deeptree97@gmail.com>
2025-03-27 23:56:12 +0900

    modify: 회원 정보 수정 메서드 작성

--- community/src/main/java/com/example/community/model/Member.java
+++ community/src/main/java/com/example/community/model/Member.java
@@ -6,29 +6,28 @@ import lombok.*;
 @Entity
 @Getter
 @Setter
+@Builder
 @NoArgsConstructor
+@AllArgsConstructor
 @Table(name="members")
-public class Member {
+public class Member extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, unique = true, length = 255)
+    @Column(nullable = false, length = 255)
     private String email;  // 이메일 (로그인 ID 역할)
 
     @Column(nullable = false, length = 255)
     private String password;  // 비밀번호 (BCrypt 해싱)
 
-    @Column(nullable = false, unique = true, length = 10)
+    @Column(nullable = false, length = 10)
     private String nickname;  // 닉네임 (최대 10자)
 
     @Column(name = "profile_image_url", length = 255, columnDefinition = "VARCHAR(255) DEFAULT 'default-image'")
     private String profileImageUrl;  // 프로필 이미지 (기본값 설정)
 
-    @Builder
-    public Member(String email, String password, String nickname, String profileImageUrl) {
-        this.email = email;
-        this.password = password;
+    public void updateProfile(String nickname, String profileImageUrl) {
         this.nickname = nickname;
         this.profileImageUrl = profileImageUrl;
     }
